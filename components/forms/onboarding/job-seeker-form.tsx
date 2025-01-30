cc998acd4bb2a5ada83ba070099d596b3a290e12
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-30 10:21:38 +0300

    Enhance job seeker form with resume upload and bio section

--- components/forms/onboarding/job-seeker-form.tsx
+++ components/forms/onboarding/job-seeker-form.tsx
@@ -1,16 +1,19 @@
 import { jobSeekerSchema } from "@/app/utils/zod-schemas";
+import { UploadDropzone } from "@/components/general/uploadthing-reexported";
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { XIcon } from "lucide-react";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -19,6 +22,8 @@ export function JobSeekerForm() {
     resolver: zodResolver(jobSeekerSchema),
     defaultValues: {
       name: "",
+      about: "",
+      resume: "",
     },
   });
 
@@ -28,16 +33,75 @@ export function JobSeekerForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <FormField
           control={form.control}
           name="name"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Name</FormLabel>
+              <FormLabel>Full Name</FormLabel>
               <FormControl>
-                <Input placeholder="Name" {...field} />
+                <Input placeholder="Enter your full name" {...field} />
               </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="about"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Short Bio</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Tell us about yourself..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="resume"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Resume</FormLabel>
+              <FormControl>
+                <div>
+                  {field.value ? (
+                    <div className="relative w-fit">
+                      <Image
+                        src={field.value}
+                        alt="Company Logo"
+                        width={100}
+                        height={100}
+                        className="rounded-lg"
+                      />
+                      <Button
+                        type="button"
+                        variant="destructive"
+                        size="icon"
+                        className="absolute -top-2 -right-2 "
+                        onClick={() => field.onChange("")}
+                      >
+                        <XIcon className="h-4 w-4" />
+                      </Button>
+                    </div>
+                  ) : (
+                    <UploadDropzone
+                      endpoint="resumeUploader"
+                      onClientUploadComplete={(res) => {
+                        field.onChange(res[0].url);
+                      }}
+                      onUploadError={() => {
+                        console.log("Error uploading image");
+                      }}
+                      className="ut-button:bg-primary ut-button:text-white dark:ut-button:text-black ut-button:hover:bg-primary/90 ut-label:text-muted-foreground ut-allowed-content:text-muted-foreground border-primary"
+                    />
+                  )}
+                </div>
+              </FormControl>
+
               <FormMessage />
             </FormItem>
           )}
