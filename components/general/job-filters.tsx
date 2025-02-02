c389ebccca3091cf599f942eadd7557ba816740f
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-02-02 15:12:39 +0300

    Enhance job filters with location selection and UI improvements

--- components/general/job-filters.tsx
+++ components/general/job-filters.tsx
@@ -3,6 +3,17 @@ import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Label } from "../ui/label";
 import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { countryList } from "@/app/utils/countries-list";
 
 const jobTypes = ["full-time", "part-time", "contract", "internship"];
 
@@ -16,7 +27,10 @@ export function JobFilters() {
           <XIcon className="size-4" />
         </Button>
       </CardHeader>
-      <CardContent>
+
+      <Separator className="mb-4" />
+
+      <CardContent className="space-y-6">
         <div className="space-y-4">
           <Label className="text-lg font-semibold">Job Type</Label>
           <div className="grid grid-cols-2 gap-4">
@@ -29,6 +43,35 @@ export function JobFilters() {
               </div>
             ))}
           </div>
+        </div>
+
+        <Separator />
+
+        <div className="space-y-4">
+          <Label className="text-lg font-semibold">Location</Label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select Location" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Worldwide</SelectLabel>
+                <SelectItem value="worldwide">
+                  <span>🌍</span>
+                  <span className="pl-2">Worldwide / Remote</span>
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>Location</SelectLabel>
+                {countryList.map((country) => (
+                  <SelectItem value={country.name} key={country.name}>
+                    <span>{country.flagEmoji}</span>
+                    <span className="pl-2">{country.name}</span>
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
       </CardContent>
     </Card>
